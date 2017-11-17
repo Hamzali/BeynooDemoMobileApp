a76bb01c5a4a4f8a2fef5ea480b33af01fc1a725
Hamza TAS <hamza.tas@getir.com>
2017-11-18 02:28:56 +0300

    dynamic test uri input is added

--- App.js
+++ App.js
@@ -3,20 +3,67 @@
  */
 
 import React, { Component } from "react";
-import { StyleSheet, View, StatusBar, WebView } from "react-native";
+import {
+  StyleSheet,
+  View,
+  StatusBar,
+  WebView,
+  TextInput,
+  Button,
+  Alert,
+  BackHandler
+} from "react-native";
 
 export default class App extends Component<{}> {
+  constructor() {
+    super();
+    this.state = {
+      isWebViewOpen: false,
+      testUri: ""
+    };
+
+    BackHandler.addEventListener("hardwareBackPress", () => {
+      if (this.state.isWebViewOpen) {
+        this.setState({ isWebViewOpen: false });
+        return true;
+      }
+    });
+  }
   render() {
     return (
       <View style={styles.container}>
-        <WebView
-          source={{
-            // Change this this value in order to test other games.
-            uri:
-              "https://hosting.backand.io/flash/flash/flashland/testpixi/index.html"
-          }}
-          style={{ height: 100, width: 1000 }}
-        />
+        {!this.state.isWebViewOpen ? (
+          <View style={{ padding: 10 }}>
+            <TextInput
+              style={{ fontSize: 18, marginBottom: 10 }}
+              placeholder={"URI/Link "}
+              value={this.state.testUri}
+              onChangeText={text => this.setState({ testUri: text })}
+            />
+            <Button
+              style={{ fontSize: 20 }}
+              title={"test game"}
+              onPress={() => {
+                if (this.state.testUri.length == 0) {
+                  return Alert.alert("Don't leave uri field empty!");
+                }
+
+                this.setState({ isWebViewOpen: true });
+              }}
+            />
+          </View>
+        ) : (
+          <WebView
+            source={{
+              uri: this.state.testUri
+            }}
+            style={{ height: 100, width: 1000 }}
+            onMessage={event => {
+              const { data } = event.nativeEvent;
+              Alert.alert(data);
+            }}
+          />
+        )}
         <StatusBar hidden={true} />
       </View>
     );
@@ -26,8 +73,6 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    justifyContent: "center"
   }
 });
